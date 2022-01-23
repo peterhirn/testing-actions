cc3cc143243c96dcbb1a691686d2f93671d9e268
Peter Hirn <peter.hirn@buildinformed.com>
2022-01-23 13:55:30 +0100

    Print process architecture

--- src/Program.fs
+++ src/Program.fs
@@ -1,9 +1,11 @@
 module App.Program
 
 open System
+open System.Runtime.InteropServices
 
 [<EntryPoint>]
 let main args =
-    printfn "Hello %A" Environment.OSVersion
+    printfn "OS: %A" Environment.OSVersion
+    printfn "Architecture: %A" RuntimeInformation.ProcessArchitecture
 
     0
