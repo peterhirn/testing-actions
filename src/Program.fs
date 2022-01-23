7ffb72f85017c640c7a53e6a5be3a9d02a1a0475
Peter Hirn <peter.hirn@buildinformed.com>
2022-01-23 20:23:08 +0100

    Forward assembly version from github release

--- src/Program.fs
+++ src/Program.fs
@@ -1,11 +1,23 @@
 module App.Program
 
 open System
+open System.Reflection
 open System.Runtime.InteropServices
+
+let assembly = lazy (Assembly.GetEntryAssembly())
+
+let version = lazy (assembly.Value.GetName().Version)
 
 [<EntryPoint>]
 let main args =
-    printfn "OS: %A" Environment.OSVersion
-    printfn "Architecture: %A" RuntimeInformation.ProcessArchitecture
+    try
+        printfn "Version: %A" version.Value
+        printfn "OS: %A" Environment.OSVersion
+        printfn "Architecture: %A" RuntimeInformation.ProcessArchitecture
+        printfn "Args: %A" args
 
-    0
+        0
+    with
+    | e ->
+        eprintfn "%A" e
+        1
